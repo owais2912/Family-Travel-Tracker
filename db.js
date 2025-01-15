d996f560598255fd33caf07d120a76335352c30f
owais2912 <owaisansari2912@gmail.com>
2025-01-15 21:22:55 +0530

    updated db file

--- db.js
+++ db.js
@@ -3,12 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = process.env;
-
-if (!DB_USER || !DB_HOST || !DB_NAME || !DB_PASSWORD || !DB_PORT) {
-	throw new Error("Missing required database environment variables");
-}
-
 const db = new pg.Pool({
 	connectionString: process.env.DATABASE_URL,
 	ssl: {
